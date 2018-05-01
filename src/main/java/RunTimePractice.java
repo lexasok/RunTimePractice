92f0c510bd4717e82509e2bfc9e96a022103a2dd
lexa <sokolov@ozero.net>
2018-05-01 22:54:21 +0300

    New method declared

--- src/main/java/RunTimePractice.java
+++ src/main/java/RunTimePractice.java
@@ -1,8 +1,18 @@
 
 
 public class RunTimePractice {
+
+    public static int findNumberOfRepeticions() {
+        return 0;
+    }
+
+
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
+
+
+
+
         long endTime = System.currentTimeMillis();
         long duration = endTime - startTime;
         System.out.println("Test " + duration);
