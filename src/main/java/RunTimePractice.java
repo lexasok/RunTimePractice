933feb04b03b7de281f93f2d2da804aaffce2695
lexa <sokolov@ozero.net>
2018-05-01 22:52:32 +0300

    Time calculated

--- src/main/java/RunTimePractice.java
+++ src/main/java/RunTimePractice.java
@@ -1,5 +1,10 @@
+
+
 public class RunTimePractice {
     public static void main(String[] args) {
-
+        long startTime = System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+        System.out.println("Test " + duration);
     }
 }
