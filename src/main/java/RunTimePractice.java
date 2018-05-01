10a844f387199c16580d0c3a0bb42bb506888432
lexa <sokolov@ozero.net>
2018-05-01 22:59:54 +0300

    Program initialized

--- src/main/java/RunTimePractice.java
+++ src/main/java/RunTimePractice.java
@@ -16,10 +16,11 @@ public class RunTimePractice {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
-        findNumberOfRepeticions("abca", 'a');
+        System.out.println(findNumberOfRepeticions("abca", 'a'));
+
 
         long endTime = System.currentTimeMillis();
         long duration = endTime - startTime;
-        System.out.println("Test " + duration);
+        System.out.println("Test " + duration + " ms");
     }
 }
