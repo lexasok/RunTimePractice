5816072d1e1c1ea61ffd221bfc0f2a83deb26a39
lexa <sokolov@ozero.net>
2018-05-02 00:49:22 +0300

    Data input initialized

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
+
+        //input data
+        Scanner sc = new Scanner(System.in);
+        int numberOfCases = sc.nextInt();
+        int[] numbers = new int[numberOfCases];
+        for (int i = 0; i < numberOfCases; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        sc.close();
+
 
     }
 }
